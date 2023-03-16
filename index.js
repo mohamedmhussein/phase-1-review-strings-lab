0a709e4399dc24ac8bd62013a668d44fc0ce94bd
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-03-16 19:36:57 -0400

    string lab completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "WELCOME TO FLATBOOK,  WELCOME TO FLATBOOK,  !";
+const welcomeMessage = "Welcome to Flatbook,  " + currentUser;
+const excitedWelcomeMessage = currentUser;
+const shortGreeting = "Welcome,  !"
